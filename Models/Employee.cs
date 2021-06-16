6c3968f01eacde7dd3000c23014aaa65097064eb
Ashraful Islam <ashraful.islam@streamstech.com>
2021-06-16 17:21:19 +0600

    Applied min length annotation

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace EmployeeApplication.API.Models
     public class Employee
     {
         public int Id { get; set; }
+        [MinLength(1)]
         public string FirstName { get; set; }
         public string MiddleName { get; set; }
         public string LastName { get; set; }
